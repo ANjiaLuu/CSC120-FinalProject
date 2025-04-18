03f8df3390740b9661aea45aa66b3bc1e682b2d4
ANjiaLuu <alu03@smith.edu>
2025-04-18 15:35:42 -0400

    NPC.java

--- NPC.java
+++ NPC.java
@@ -1,1 +1,17 @@
+public class NPC{
+  protected String name;
+  protected String description;
+  protected String dialogue;
+  public NPC(String name,String description,String dialogue){
+    this.name=name;
+    this.description=description;
+    this.dialogue=dialogue;
+  public talk(){
+    System.out.println(name+dialogue)
+  }
+  public String getName{
+    return this.name;
 
+  public String getDescription{
+    return this.description;
+}
