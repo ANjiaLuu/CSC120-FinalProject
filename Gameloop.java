101ce62c7ded94e1c91f2f8bb817f9a4c3e1d22d
ANjiaLuu <alu03@smith.edu>
2025-04-19 11:07:45 -0400

    Gameloop.java

--- Gameloop.java
+++ Gameloop.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Gameloop{
   public static void main(String[] args){
-    NPC Shakila=Npc("")
+    String [] shakila_d={};
+    NPC Shakila=NPC("Shakila","Works in Seelye basement, and has access to every door on campus with Seelye OneCard. Victim copied Shakila’s homework and accusations of plagiarism nearly got her to the honor board. :( 
+", Array.asList(shakila_d), Location Seelye);
   }
 }
 
