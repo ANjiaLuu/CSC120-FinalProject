4ffad2e57244ab893f0ce3261de05bcd15ff7234
ANjiaLuu <alu03@smith.edu>
2025-04-19 03:03:11 -0400

    Command.java

--- Command.java
+++ Command.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class Command{
+  private Player player
+  private Map map;
+
+  public Command(){
+    this.player=player;
+    this.map=map;
+  }
+  public String interpret(String input){
+    
+  }
+}
+
 
