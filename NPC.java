45240353971d688a8834af6ad17444340ba11406
ANjiaLuu <alu03@smith.edu>
2025-04-19 02:38:30 -0400

    NPC.java

--- NPC.java
+++ NPC.java
@@ -2,15 +2,24 @@ public class NPC{
   protected String name;
   protected String description;
   protected String dialogue;
-  public NPC(String name,String description,String dialogue){
+  protected Location location;
+  public NPC(String name,String description,String dialogue,Location,location){
     this.name=name;
     this.description=description;
     this.dialogue=dialogue;
+    this.location=location;
   public talk(){
     System.out.println(name+dialogue)
   }
   public String getName{
     return this.name;
+  }
+  public getLocation{
+    return this.location;
+  }
+  public move(Location newLocation){
+    this.location=newlocation;
+  }
 
   public String getDescription{
     return this.description;
