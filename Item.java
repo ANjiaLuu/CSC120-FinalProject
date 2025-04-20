dd1322a897080a9d618722283be0124a702100d8
Savannah0215 <sko65@smith.edu>
2025-04-19 20:34:43 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.Collection;
 public class Item extends Coordinates {
   String name;
   x: double;
@@ -5,7 +7,9 @@ public class Item extends Coordinates {
   String itemDescription;
   boolean hasBeenPickedUp;
   boolean isInsideAnotherItem;
+  Collection<String> collectionOfItems = new ArrayList<String>();
 
+  public 
   public Item(String n, double x, double y, String d, boolean i){
     this.name = n;
     this.x = x;
@@ -13,6 +17,31 @@ public class Item extends Coordinates {
     this.itemDescription = d;
     this.hasBeenPickedUp = false;
     this.isInsideAnotherItem = i;
+  }
+
+  public showDescription(String[] args){
+    Scanner scansForItem = new Scanner(System.in);
+    String userInput = scansForItem.nextLine();
+    switch(userInput contains "inspect") && (userInput contains ListOfItems){
+      case NEWSPAPER:
+        System.out.println(newspaper.itemDescription);
+        break;
+      case EPIPEN:
+        System.out.println(epipen.itemDescription);
+        break;
+      case ESSAY:
+        System.out.println(essay.itemDescription);
+        break;
+      case BACKPACK:
+        System.out.println(backpack.itemDescription);
+        break;
+      case PHOTO:
+        System.out.println(photo.itemDescription);
+        break;
+      case CAKE:
+        System.out.println(cake.itemDescription);
+        break;
+    }
   }
 /**
    * Item descriptions have not been written yet.
@@ -24,5 +53,13 @@ public class Item extends Coordinates {
     Item backpack = new Item("Boba's Backpack", 1, 2, "", false);
     Item photo = new Item("Photo", 1, 2, "", true);
     Item cake = new Item("Cake", 0, 1, "", false);
+    enum ListOfItems {
+      NEWSPAPER,
+      EPIPEN,
+      ESSAY,
+      BACKPACK,
+      PHOTO,
+      CAKE
+    }
   }
 }
