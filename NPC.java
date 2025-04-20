019c35bb2a7744b56db9c91fe96da280e7f4f62f
ANjiaLuu <alu03@smith.edu>
2025-04-19 22:27:56 -0400

    NPC.java

--- NPC.java
+++ NPC.java
@@ -3,26 +3,32 @@ public class NPC{
   protected String description;
   protected String dialogue;
   protected Coordinates coordinates;
-  public NPC(String name,String description,String dialogue,Location,location){
+  public NPC(String name,String description,String dialogue,Coordinates coordinates){
     this.name=name;
     this.description=description;
     this.dialogue=dialogue;
-    this.location=location;
+    this.coordinates=coordinates;
+  }
+	
   public void talk(){
     System.out.println(name+":"+dialogue);
   }
-  public String getName{
+	
+  public String getName(){
     return this.name;
   }
-  public Coordinates getCoordinates{
+	
+  public Coordinates getCoordinates(){
     return this.coordinates;
   }
+	
   public void move(Coordinates newCoordinates){
     this.coordinates=newCoordinates;
   }
 
   public String getDescription{
     return this.description;
+  }
     
   public static void main(String[] args){
     String [] shakila_d={ “Look closely at the EpiPen - it was broken! Someone messed with it so it wouldn’t work properly.”
@@ -30,7 +36,7 @@ public class NPC{
 };
  
     NPC Shakila=NPC("Shakila","Works in Seelye basement, and has access to every door on campus with Seelye OneCard. Victim copied Shakila’s homework and accusations of plagiarism nearly got her to the honor board. :( 
-", Array.asList(shakila_d), Coordinates Seelye_Basement);
+", Array.asList(shakila_d), seelyeBasement);
 
     
     String [] hanyu_d={“I think it was Savannah! She was acting weird, asking me about peanut allergies when I don’t do food science, I do other things in Chemistry. If you want to look for Savannah I think she’s working a shift in Lamont dining hall. Also my lab is nearly over so I’m going to go back to my House.”
@@ -39,12 +45,17 @@ public class NPC{
 };
     NPC Hanyu=NPC("Hanyu","lives in Talbot House, just like the victim! Maybe Hanyu wanted her room next year.
 	
-", Array.asList(hanyu_d), Coordinates Chemistry_Lab);
+", Array.asList(hanyu_d),chemistryLab);
 
   
    String [] savannah_d={ “Look at the newspaper! There’s a photo of me in Ceramics Club during the night of the murder. I have to be innocent!”};
-     NPC Savanah=NPC("Savanah","Works in Lamont Dining Hall, just like the victim. Didn’t like how lazy they were in the dining hall. Had a chance to poison the student with peanuts!
-",Array.asList(savannah_d),Coordinates Lamont_Dining);
-  }
-  }
+   NPC Savanah=NPC("Savanah","Works in Lamont Dining Hall, just like the victim. Didn’t like how lazy they were in the dining hall. Had a chance to poison the student with peanuts!
+",Array.asList(savannah_d),lamontDining);
+
+   String [] anjia_d={”There is no way I will kill Boba! I always run with her and she is awesome!”
+,“We weren’t really friends when Boba passed away, but that doesn’t mean I killed her. She died from a peanut poisoning, right? I had no way of poisoning her.”
+};
+   NPC Anjia=NPC("Anjia","Was on the running club just like the victim. Jealous of her mile time.
+",Array.asList(anjia_d),outdoorTrack);
+  
 }
