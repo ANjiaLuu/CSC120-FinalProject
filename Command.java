0ed857f240384ad63060fab424b65c7cdc555b44
Savannah0215 <sko65@smith.edu>
2025-04-22 10:18:40 -0400

    Update Command.java

--- Command.java
+++ Command.java
@@ -1,18 +1,38 @@
 import java.util.Scanner;
-public class Command{
+public class Command extends Item {
   private Player player
   private Map map;
+  boolean gameIsRunning = true;
 
   public Command(){
     this.player=player;
     this.map=map;
   }
-  public String interpret(String input){
-    if (input==null){
-      return "You need to enter a command.";
+  while gameIsRunning {
+    public String interpret(String input){
+      if (input==null){
+        return "You need to enter a command.";
+        continue
     }
-    
-  }
+      switch(userInput contains "inspect") && (userInput contains ListOfItems){
+        case NEWSPAPER:
+          System.out.println(newspaper.itemDescription);
+          break;
+        case EPIPEN:
+          System.out.println(epipen.itemDescription);
+          break;
+        case ESSAY:
+          System.out.println(essay.itemDescription);
+          break;
+        case BACKPACK:
+          System.out.println(backpack.itemDescription);
+          break;
+        case PHOTO:
+          System.out.println(photo.itemDescription);
+          break;
+        case CAKE:
+          System.out.println(cake.itemDescription);
+          break;
 }
 
 
