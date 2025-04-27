ee3b46e51964a9b72bc78b7464dd0d87d77bc3b7
Savannah0215 <sko65@smith.edu>
2025-04-27 11:18:10 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -7,8 +7,8 @@ public class Item extends Coordinates {
   String itemDescription;
   boolean hasBeenPickedUp;
   boolean isInsideAnotherItem;
-  Collection<String> collectionOfItems = new ArrayList<String>();
-
+  ArrayList<Item> listOfItems = new ArrayList<Item>();
+/**
   public enum ListOfItems {
       NEWSPAPER("Newspaper"),
       EPIPEN("Epipen"),
@@ -25,6 +25,7 @@ public class Item extends Coordinates {
       public String displayName() {
         return displayName; 
       }
+  */
     
   public Item(String n, double x, double y, String d, boolean i){
     this.name = n;
@@ -34,17 +35,15 @@ public class Item extends Coordinates {
     this.hasBeenPickedUp = false;
     this.isInsideAnotherItem = i;
   }
-/**
-   * Item descriptions have not been written yet.
-   **/
   public static void main {
     Item newspaper = new Item("Newspaper", 1, 1, "Today's issue of the Sophian. Its main headline states 'Beloved Student Boba T. Slain Last Night in Talbot House!' Underneath it, there's another story about the Ceramics Club enjoying their pottery-making. Savannah is mentioned in this story.", false);
     Item epipen = new Item("EpiPen", 0, 2, "A medication used to treat allergic reactions. It seems like Boba used this sometime ago.", false);
     Item essay = new Item("Shakila's Essay", 2, 2, "A five-page essay. The very top of it says 'Shakila, this is the same paper that Boba give me! See me after class.'", false);
-    Item backpack = new Item("Boba's Backpack", 1, 2, "There's a photo inside the front pocket of the backpack! Apart from that, the backpack appears to be empty.", false);
+    Item backpack = new Item("Boba's Backpack", 1, 2, "There's a photo inside the front pocket of the backpack! You should be able to pick up this photo. Apart from that, the backpack appears to be empty.", false);
     Item photo = new Item("Photo", 1, 2, "A nice Polaroid picture of Boba, Shakila, Anjia, Savannah, and Hanyu. But it seems like Anjia's face has been written over with black marker.", true);
     Item cake = new Item("Cake", 0, 1, "A slice of vanilla cake on a paper plate. It doesn't seem like there are allergens in here, but you'd have to check...", false);
 
+    
     }
   }
 }
