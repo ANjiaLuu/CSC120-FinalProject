c15e56f732af332274de09bc73dae4e85ef4e28c
Savannah0215 <sko65@smith.edu>
2025-04-27 10:56:38 -0400

    Update Item.java
    No coding done. Just a written description for all 6 items in the story.

--- Item.java
+++ Item.java
@@ -38,12 +38,12 @@ public class Item extends Coordinates {
    * Item descriptions have not been written yet.
    **/
   public static void main {
-    Item newspaper = new Item("Newspaper", 1, 1, "", false);
-    Item epipen = new Item("EpiPen", 0, 2, "", false);
-    Item essay = new Item("Shakila's Essay", 2, 2, "", false);
-    Item backpack = new Item("Boba's Backpack", 1, 2, "", false);
-    Item photo = new Item("Photo", 1, 2, "", true);
-    Item cake = new Item("Cake", 0, 1, "", false);
+    Item newspaper = new Item("Newspaper", 1, 1, "Today's issue of the Sophian. Its main headline states 'Beloved Student Boba T. Slain Last Night in Talbot House!' Underneath it, there's another story about the Ceramics Club enjoying their pottery-making. Savannah is mentioned in this story.", false);
+    Item epipen = new Item("EpiPen", 0, 2, "A medication used to treat allergic reactions. It seems like Boba used this sometime ago.", false);
+    Item essay = new Item("Shakila's Essay", 2, 2, "A five-page essay. The very top of it says 'Shakila, this is the same paper that Boba give me! See me after class.'", false);
+    Item backpack = new Item("Boba's Backpack", 1, 2, "There's a photo inside the front pocket of the backpack! Apart from that, the backpack appears to be empty.", false);
+    Item photo = new Item("Photo", 1, 2, "A nice Polaroid picture of Boba, Shakila, Anjia, Savannah, and Hanyu. But it seems like Anjia's face has been written over with black marker.", true);
+    Item cake = new Item("Cake", 0, 1, "A slice of vanilla cake on a paper plate. It doesn't seem like there are allergens in here, but you'd have to check...", false);
 
     }
   }
